7399225fcea8e9b9e92acf55b79e53cb15564776
shengchao <33279207@qq.com>
2020-11-02 14:45:49 +0800

    后台系统 添加登录拦截中间件

--- src/module/admin/login/login.controller.ts
+++ src/module/admin/login/login.controller.ts
@@ -1,10 +1,23 @@
-import { Controller,Get, Render } from '@nestjs/common';
+import { ToolsService } from './../../../service/tools/tools.service';
+import { Controller,Get, Render,Request,Response } from '@nestjs/common';
 
 @Controller('admin/login')
 export class LoginController {
+
+    constructor(private toolsService :ToolsService){}
+
     @Get()
     @Render('admin/login')
     index() {
         return {};
+    }
+
+    @Get('code')
+    code(@Request() req, @Response() res) {
+        var svgCaptcha = this.toolsService.getCaptcha();
+        // 设置session
+        req.session.captcha = svgCaptcha.text;
+        res.type('image/svg+xml');
+        res.send(svgCaptcha.data);
     }
 }
--- src/service/tools/tools.service.ts
+++ src/service/tools/tools.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+// 引入验证码库
+import * as svgCaptcha from 'svg-captcha';
 
 @Injectable()
-export class ToolsService {}
+export class ToolsService {
+    getCaptcha() {
+        var captcha = svgCaptcha.create({
+            size: 4,
+            fontSize: 50,
+            width: 100,
+            height: 34,
+            background: "#cc9966"
+        });
+        return captcha;
+    }
+}
